0e3729d63ee2433c7747c13471530f2d36a9d358
Vishesh soni <sonivishesh099@gmail.com>
2026-04-22 12:02:55 +0530

    Time: 26 ms (93.09%), Space: 49.4 MB (7.28%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,11 +1,20 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        int n = candyType.length;
-        int temp = n/2;
-        Set<Integer> set = new HashSet<>();
-        for(int i: candyType){
-            set.add(i);
+        // int n = candyType.length;
+        // int temp = n/2;
+        // Set<Integer> set = new HashSet<>();
+        // for(int i: candyType){
+        //     set.add(i);
+        // }
+        // return Math.min(temp, set.size());
+
+        int n = candyType.length; 
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            if(!map.containsKey(candyType[i]) && map.size()<(n/2)){
+                map.put(candyType[i], 1);
+            }
         }
-        return Math.min(temp, set.size());
+        return map.size();
     }
 }
