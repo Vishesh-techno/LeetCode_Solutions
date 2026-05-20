c935f0cf3cbbc1c6c61421aad9268fca1a66749f
Vishesh soni <sonivishesh099@gmail.com>
2026-05-20 20:42:58 +0530

    Time: 34 ms (60.31%), Space: 59.2 MB (44.96%) - LeetHub - Time: O(?), Space: O(?)

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: sort the array
+
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             int left = i + 1, right = nums.length - 1;
+            int target = -nums[i];
+
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
                     left++;
                     right--;
-                } else if (sum < 0) {
+                } else if (sum < target) {
                     left++;
                 } else {
                     right--;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
