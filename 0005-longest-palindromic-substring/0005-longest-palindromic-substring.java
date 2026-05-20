5f9f17ad9cce91ce8e64dc495c4eb9c475585414
Vishesh soni <sonivishesh099@gmail.com>
2026-05-20 20:13:58 +0530

    Time: 2561 ms (5%), Space: 47 MB (17.89%) - LeetHub - Time: O(?), Space: O(?)

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
         String max = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String str = s.substring(i, j + 1);
-
-                if (isPalindrome(str) && str.length() > max.length()) {
+        for(int i=0; i<s.length(); i++){
+            for(int j=i; j<s.length(); j++){
+                String str = s.substring(i, j+1);
+                if(isPalindrome(str) && str.length() > max.length()){
                     max = str;
                 }
             }
         }
         return max;
     }
-
-    public static boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left <= right) {
-            if (s.charAt(left) != s.charAt(right)) {
+    public static boolean isPalindrome(String s){
+        int left = 0, right = s.length()-1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            left++;
-            right--;
+            left++; right--;
         }
         return true;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
